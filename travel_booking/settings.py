99b02ccdf7d0caa65a5cbdf277d07989f96024ba
SufiyanShareef <mdsufiyanshareef@gmail.com>
2025-08-28 20:53:28 +0530

    travel booking app

--- travel_booking/settings.py
+++ travel_booking/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 from pathlib import Path
 from dotenv import load_dotenv
 import os
+import pymysql
+pymysql.install_as_MySQLdb()
 
 load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
