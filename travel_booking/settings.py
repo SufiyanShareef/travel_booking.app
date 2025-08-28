953eb9ff5ccb6ba142c998090aa00b105742bf79
SufiyanShareef <mdsufiyanshareef@gmail.com>
2025-08-28 21:07:46 +0530

    final -maybe

--- travel_booking/settings.py
+++ travel_booking/settings.py
@@ -28,9 +28,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG', 'False').lower() in ['true', '1', 't', 'y', 'yes']
-
+# # SECURITY WARNING: don't run with debug turned on in production!
+# DEBUG = os.environ.get('DEBUG', 'False').lower() in ['true', '1', 't', 'y', 'yes']
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
@@ -129,7 +129,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL =  BASE_DIR / 'static/'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [BASE_DIR / 'static']   # Your static folder during dev
+STATIC_ROOT = BASE_DIR / 'staticfiles'     # Where collectstatic will dump files in production
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
